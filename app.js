0d4f7e3e9c9cdb72b09ea4c63338d8659da0163e
Yasmin <Yasmin.Kotb21@gmail.com>
2024-05-06 13:37:23 +0300

    init

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express=require('express')
+const app=express()
+
+//middleware 
+
+app.use(express.json())
+const port=process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+
+    res.status(200).send("hello")
+})
+app.listen(port,()=>{
+    console.log("Server Started :)");
+})
